5d1335ae81a38c70ccbaf0c1beb810615d57e792
nikhildarocha <niks77883@gmail.com>
2020-09-26 13:25:09 +0530

    @ndarocha ex1.3 pie chart

--- ex1.3_bar_graph.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import  pyplot as plt
-
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-cities = ['Philadelphia', 'Suburbs', 'PA', 'NJ', 'Elsewhere']
-students = [225, 100, 60, 75, 40]
-fig, ax = plt.subplots(figsize =(10, 7)) 
-ax.bar(cities,students)
-plt.show()
