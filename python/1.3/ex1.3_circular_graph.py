1d289b66f04ab8a440f093c45e83ff90944282b5
nikhildarocha <niks77883@gmail.com>
2020-09-26 13:38:52 +0530

    @ndarocha ex1.3 pie chart simple

--- python/1.3/ex1.3_circular_graph.py
+++ python/1.3/ex1.3_circular_graph.py
@@ -3,12 +3,9 @@ import matplotlib.pyplot as plt
 # Data to plot
 labels = 'Philadelphia', 'Suburbs', 'PA', 'NJ', 'Elsewhere'
 sizes = [225, 100, 60, 75, 40]
-colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
-explode = (0.1, 0, 0, 0, 0)  # explode 1st slice
 
-# Plot
-plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-autopct='%1.1f%%', shadow=True, startangle=140)
-
-plt.axis('equal')
-plt.show()
+fig = plt.figure(figsize =(10, 7)) 
+plt.pie(sizes, labels = labels, autopct='%1.0f%%') 
+  
+# show plot 
+plt.show() 
